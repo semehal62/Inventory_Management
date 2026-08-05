00bab7c7ad539b7066ec6907ee1531cc0176fd6f
semehal62 <semehalhailu4@gmail.com>
2026-08-05 09:40:05 +0300

    (Database modification): Add Baseuser Table

--- Inventory management System/Models/AI_Status.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Inventory_management_System.Models
-{
-    public enum AI_Status
-    {
-        Approved  = 1,
-        Warning = 2,
-        Flagged = 3
-    }
-}
--- Inventory management System/Models/Manager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Inventory_management_System.Models
-{
-    public class Manager 
-    {
-        public int Id { get; set; }
-        public BaseUser? User { get; set; }
-        public required int  BaseUserId { get; set; }
-    }
-}
